ad82d4145c201b642b729766c068e285c1491456
Bernadette <chukwuedoogochukwubernadette@gmail.com>
2022-10-13 13:13:46 -0700

    commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,7 @@
-#include <stdio.h> 
+#include <stdio.h>
+/*
+ * print the data types
+ */
 int main(void)
 {        printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
