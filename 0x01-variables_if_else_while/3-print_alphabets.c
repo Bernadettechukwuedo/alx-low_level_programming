d378fa8dc8959a32397804443409ae16cc60c8a4
Bernadette <chukwuedoogochukwubernadette@gmail.com>
2022-10-14 15:34:52 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char alphabet;
+	char alphABET;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+	putchar(alphabet);
+	}
+
+	for (alphABET = 'A'; alphABET <= 'Z'; alphABET++)
+	{
+	putchar(alphABET);
+	}
+
+
+	putchar('\n');
+	return (0);
