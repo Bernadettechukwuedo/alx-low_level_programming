6dbff9051444f83143f69c4e6e538ede96d8f555
Bernadette <chukwuedoogochukwubernadette@gmail.com>
2022-10-14 15:58:02 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+	{
+	printf("%i", number);
+	}
+	
+	printf('\n');
+
+	return (0);
+}
