5983cf968aaad5ea6d172891551d5ad4d68a4772
Bernadette <chukwuedoogochukwubernadette@gmail.com>
2022-11-02 11:25:45 -0700

    recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,14 +1,16 @@
 #include "main.h"
+
 /**
  * _strlen_recursion - returns the length of a string
- * @s : string
- * Return: 0
+ * @s: string input
+ * Return: length of s
  */
+
 int _strlen_recursion(char *s)
 {
 	if (*s)
 	{
-	_strlen_recursion(*s);
-	_putchar(*s);
+		return (1 + _strlen_recursion(s + 1));
 	}
+	return (0);
 }
