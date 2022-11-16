01799986b2053a9abb0092dc4fa0e5351ca4ade2
Bernadette <chukwuedoogochukwubernadette@gmail.com>
2022-11-16 03:33:16 -0800

    pointers to function

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,10 +3,10 @@
  * print_name - prints a name
  * @name : placeholder for name
  * @f : pointer for printing the function
+ * Return: no return
  */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
-		return;
-	f(name);
+		f(name);
 }
